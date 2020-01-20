19104f94e818c6c2b07fe6720230bd6f9be3f9ce
Kersa Andika <kersakeun@gmail.com>
2020-01-20 21:32:17 +0700

    push to repo

---
+++ theme-control.js
@@ -0,0 +1,35 @@
+// This will called when the site is loaded
+window.onload = (event) => {
+  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
+  // First check on color scheme based on user session storage
+  if(sessionStorage.getItem('theme') === 'dark' || 'light')
+    changeTheme(sessionStorage.getItem('theme'))
+  else {
+    if(mediaQuery.matches)
+      changeTheme('dark')
+    else 
+      changeTheme('light')
+  }
+  
+  // Add listener for real time user colour scheme preference change
+  mediaQuery.addListener(e => {
+      if(e.matches)
+        changeTheme('dark')
+      else 
+        changeTheme('light')
+  })
+}
+
+// Function to toggle the theme
+function toggleTheme() {
+  if (document.documentElement.getAttribute('data-theme') === 'dark') 
+    changeTheme('light')
+  else 
+    changeTheme('dark')
+}
+
+// Function to change the theme
+function changeTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme)
+  sessionStorage.setItem('theme', theme) // Set user theme on session storage
+}
